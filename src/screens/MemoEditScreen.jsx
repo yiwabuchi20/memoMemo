bf15b28a3645a9a1d5213a42699176c8d5663559
yiwabuchi <yiwabuchi20@gmail.com>
2022-08-08 15:45:26 +0900

    feat: 画面を作成した

---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { TextInput, StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import CircleButton from '../components/CircleButton';
+import MemoAppHeader from '../components/MemoAppHeader';
+
+export default function MemoEditScreen() {
+  const [text, setText] = useState();
+  return (
+    <>
+      <StatusBar />
+      <MemoAppHeader title="MemoApp" showBack />
+      <KeyboardAvoidingView style={styles.container} behavior="height">
+        <View style={styles.inputContainer}>
+          <TextInput value={text} onChangeText={setText} style={styles.input} multiline />
+        </View>
+        <CircleButton onPress={() => {}} name={'check'} style={{ right: 30, bottom: 40 }} />
+      </KeyboardAvoidingView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    flex: 1,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
